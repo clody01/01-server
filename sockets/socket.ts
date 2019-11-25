2ccccfbacbac03c762daff218dec70ed661fa984
clody01 <tseoue01@gmail.com>
2019-11-25 19:25:14 +0100

    Section 4

---
+++ sockets/socket.ts
@@ -0,0 +1,15 @@
+import {Socket} from 'socket.io';
+import socketIO from 'socket.io';
+
+
+export const disconnect = (client: Socket) => {
+    client.on('disconnect', () => {
+        console.log('Client disconnected!');
+    })
+};
+export const messageListener = (client: Socket, io: socketIO.Server) => {
+    client.on('message', (payload: { from: string, body: string }) => {
+        console.log('message received ', payload);
+        io.emit('new-message',payload);
+    })
+};
